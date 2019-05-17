5b7277a2c1a010938e5113ea11054e15293b7b06
ojgenn <ojgenn@gmail.comgit>
2019-05-18 00:36:14 +0300

    add favorites and 404 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { SearchComponent } from './pages/search/search.component';
+import { FavoritesComponent } from './pages/favorites/favorites/favorites.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SearchComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import {
   MatAutocompleteModule, MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatRadioModule,
@@ -15,14 +17,16 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { FavoritesComponent } from './pages/favorites/favorites/favorites.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const COMPONENTS = [
   AppComponent,
   SearchComponent,
   HeaderComponent,
   SearchResultsComponent,
+  FavoritesComponent,
+  PageNotFoundComponent,
 ];
 
 const IMPORTS = [
